0c7a01d1909a8367da7e6d6c07ba4726435fd5b5
ryane <ryanesc@gmail.com>
2014-05-26 09:19:20 -0400

    integrated browserSync workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,25 @@
 var gulp = require('gulp'),
-    notify = require('gulp-notify'),
     clean = require('gulp-clean'),
     cp = require('child_process'),
     sass = require('gulp-ruby-sass'),
     rename = require('gulp-rename'),
-    minifycss = require('gulp-minify-css');
+    minifycss = require('gulp-minify-css')
+    browserSync = require('browser-sync');
 
 var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build',
+    stylesBuild: '<span style="color: grey">Running:</span> styles',
+    jekyllBuildCompleted: '<span style="color: grey">Completed:</span> $ jekyll build'
 };
 
 gulp.task('styles', function() {
+  browserSync.notify(messages.stylesBuild);
   return gulp.src('_scss/main.scss')
     .pipe(sass({ style: 'expanded' }))
     .pipe(gulp.dest('css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
-    .pipe(gulp.dest('css'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('clean', function() {
@@ -28,14 +30,27 @@ gulp.task('clean', function() {
 gulp.task('watch', function() {
   gulp.watch('_scss/**/*.scss', ['styles']);
   gulp.watch(['_layouts/*.html', '_posts/*', '_config.yml', 'css/*'],
-             ['jekyll-build']);
+             ['jekyll-rebuild']);
 });
 
 gulp.task('jekyll-build', function (done) {
+  browserSync.notify(messages.jekyllBuild);
   return cp.spawn('jekyll', ['build'], { stdio: 'inherit' })
     .on('close', done);
 });
 
-gulp.task('default', [ 'clean' ], function() {
-  gulp.start('styles', 'jekyll-build')
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+  browserSync.notify(messages.jekyllBuildCompleted);
+  browserSync.reload();
 });
+
+gulp.task('browser-sync', ['styles', 'jekyll-build'], function() {
+  browserSync.init(null, {
+    server: {
+      baseDir: '_site'
+    },
+    host: "localhost"
+  });
+});
+
+gulp.task('default', [ 'clean', 'browser-sync', 'watch' ]);
